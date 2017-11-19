0f8a49c7d882a4f4553a3b5ab581ff776df59e7c
fecigindev <33804459+fecigindev@users.noreply.github.com>
2017-11-19 04:52:02 -0800

    Update show_table_inDB.php

--- show_table_inDB.php
+++ show_table_inDB.php
@@ -1,9 +1,9 @@
 <?php
-$con = mysqli_connect("127.0.0.1","root","","mydatabase");
-$sql = "SELECT * FROM yourTable;
-$result = $con->query($sql);
+$sambung = mysqli_connect("127.0.0.1","root","","mydatabase");
+$sql = "SELECT * FROM yourTable";  //contoh SELECT * FROM perpus
+$result = $sambung->query($sql);
 
 while($r = $result->fetch_assoc()){
-  echo $r['colum']," ",$r['row'];
+  echo $r['colum']," ",$r['row'];     //contoh $r['alamat'] $r['nama depan']
 }
 ?>
