e1461e3a03baaf9c5cc46a02cfb7a292abb01087
fecigindev <33804459+fecigindev@users.noreply.github.com>
2017-11-19 04:49:17 -0800

    Update show_table_inDB.php

--- show_table_inDB.php
+++ show_table_inDB.php
@@ -1,3 +1,9 @@
 <?php
+$con = mysqli_connect("127.0.0.1","root","","mydatabase");
+$sql = "SELECT * FROM yourTable;
+$result = $con->query($sql);
 
+while($r = $result->fetch_assoc()){
+  echo $r['colum']," ",$r['row'];
+}
 ?>
