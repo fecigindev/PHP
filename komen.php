a6ac8100fee654befffcb964a4ab289c856af8c4
fecigindev <33804459+fecigindev@users.noreply.github.com>
2017-11-23 05:07:02 -0800

    Create komen.php

---
+++ komen.php
@@ -0,0 +1,7 @@
+<?php
+    echo 'ini test output di browser'; // komen ini sama seperti bahasa c++
+    /* ini multi komen php
+       ini line kedua */
+    echo 'ini test 2';
+    echo 'ini output terakhir'; # komen ini hanya satu line atau satu baris
+?>
